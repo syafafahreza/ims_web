8d43d80aeedce814a7184c0e0637488296be00dc
syafa fahreza <syafafahreza7@gmail.com>
2023-01-04 00:00:21 +0700

    Added docstring to transaction_view

--- ims_django/transaction/views.py
+++ ims_django/transaction/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import *
 @login_required(login_url='login')
 @permission_required('transaction.view_transaction_history')
 def transaction_view(request):
+    '''
+        Return transaction history page.
+    '''
     page_title = 'Transaction'
     context = {'title': page_title}
     return render(request, 'transaction.html', context)
